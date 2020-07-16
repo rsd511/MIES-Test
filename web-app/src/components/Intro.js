81d08fd3c235509bd197d936d727f310ccb4a4bb
Rahul Sawantdesai <rahul.sawant17@siesgst.ac.in>
2020-07-16 20:37:21 +0530

    Improvised data cleaning & analysis

--- web-app/src/components/Intro.js
+++ web-app/src/components/Intro.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import '../styles/Intro.css'
 
 function Intro() {
     return (
         <div className="Intro">
-            Introduction
+            <div className="Prompt">
+                <p>
+                    Ever wondered if you are the life of the party? Or if you prefer minimally stimulating environments? Or a little bit of both?!
+                </p>
+                <button>
+                    Take the test!
+                </button>
+            </div>
+            <div className="Definition">
+                <div className="card">
+                    1
+                </div>
+                <div className="card">
+                    2
+                </div>
+                <div className="card">
+                    3
+                </div>
+            </div>
         </div>
     )
 }
