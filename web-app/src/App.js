d638bf20a6c4861d5c56d8ddaecf8c54711998f9
Rahul Sawantdesai <rahul.sawant17@siesgst.ac.in>
2020-07-29 14:33:19 +0530

    Completed Test Page

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import './App.css';
 
 import Header from './components/Header';
--- web-app/src/components/Test.js
+++ web-app/src/components/Test.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-
 import './Test.css';
+
+import Queries from './Queries';
 
 class Test extends Component {
 
@@ -10,20 +11,16 @@ class Test extends Component {
     },
     currentPage : 0,
     age : 1,
-    gender : 0
+    gender : 0,
+    response : {}
   }
 
-  changePage = () => {
-    let newProgress = {}
-
-    for(let i = 0; i <= 9; i++) {
-      if(i < this.state.currentPage)
-        newProgress[i] = 1;
-      else
-        newProgress[i] = 0;
-    }
-
-    this.setState({ progress : newProgress });
+  componentDidMount() {
+    let dummy = {}
+    Object.keys(Queries).forEach(key => {
+      dummy[key] = 3;
+    });
+    this.setState({ response : dummy });
   }
 
   changeAge = (event) => {
@@ -42,11 +39,65 @@ class Test extends Component {
     event.preventDefault();
   }
 
+  changeAnswer = (event) => {
+    let dummy = this.state.response;
+    dummy[event.target.name] = parseInt(event.target.value)
+    this.setState({ response : dummy });
+  }
+
+  changePage = () => {
+    let newProgress = {}
+
+    for(let i = 0; i <= 9; i++) {
+      if(i < this.state.currentPage)
+        newProgress[i] = 1;
+      else
+        newProgress[i] = 0;
+    }
+
+    this.setState({ progress : newProgress });
+  }
+
+  goBack = () => {
+    let newpage = this.state.currentPage - 1;
+    let newProgress = {}
+
+    for(let i = 0; i <= 9; i++) {
+      if(i < newpage)
+        newProgress[i] = 1;
+      else
+        newProgress[i] = 0;
+    }
+
+    this.setState({ progress : newProgress });
+    this.setState({currentPage : newpage});
+  }
+
+  goNext = () => {
+    let newpage = this.state.currentPage + 1;
+    let newProgress = {}
+
+    for(let i = 0; i <= 9; i++) {
+      if(i < newpage)
+        newProgress[i] = 1;
+      else
+        newProgress[i] = 0;
+    }
+
+    this.setState({ progress : newProgress });
+    this.setState({currentPage : newpage});
+  }
+
   render() {
 
     const basicStyle = {
       color : this.props.theme.main,
       backgroundColor : this.props.theme.background,
+      borderColor : this.props.theme.main
+    }
+    const inverseStyle = {
+      color : this.props.theme.background,
+      backgroundColor : this.props.theme.main,
       borderColor : this.props.theme.main
     }
 
@@ -68,32 +119,116 @@ class Test extends Component {
     for(let i = 0; i <= 9; i++) {
       progressLists.push(
         <li style = {progressSlot[i]} key = {i} >-</li>
-      )
+      );
+    }
+
+    let page = [[], [], [], [], [], [], [], [], [], []]
+    page[0].push(
+      <div key = "basic1" style = {basicStyle} className = "Basic" >
+        <label>Your age : </label>
+        <input style = {basicStyle} onChange = {this.changeAge} type = "number" value = {this.state.age} min = {1} max = {150} />
+      </div>
+    );
+    page[0].push(
+      <div key = "basic2" style = {basicStyle} className = "Basic" >
+        <label>Your gender : </label>
+        <select style = {basicStyle} onChange = {this.changeGender} value = {this.state.gender}>
+          <option value={0}>Rather not say</option>
+          <option value={1}>Male</option>
+          <option value={2}>Female</option>
+          <option value={3}>Other</option>
+        </select>
+      </div>
+    );
+
+    for(let i = 1; i <= 9; i++) {
+      let id = i * 10 - 10;
+      let ide = id + 9;
+      if(i === 9) ide = ide + 1;
+      for(let j = id; j <= ide; j++) {
+        page[i].push(
+          <div key = {Object.keys(Queries)[j]} className = "Question" style = {basicStyle} >
+            <label> {Object.values(Queries)[j]} </label>
+            <hr style = {basicStyle} ></hr>
+            <ul>
+              <li>
+                <input onChange = {this.changeAnswer} type = "radio" name = {Object.keys(Queries)[j]} value = {1} checked = {this.state.response[Object.keys(Queries)[j]] === 1}  />
+                <br></br>
+                Disagree
+              </li>
+              <li>
+                <input onChange = {this.changeAnswer} type = "radio" name = {Object.keys(Queries)[j]} value = {2} checked = {this.state.response[Object.keys(Queries)[j]] === 2}  />
+                <br></br>
+                Slightly disagree
+              </li>
+              <li>
+                <input onChange = {this.changeAnswer} type = "radio" name = {Object.keys(Queries)[j]} value = {3} checked = {this.state.response[Object.keys(Queries)[j]] === 3}  />
+                <br></br>
+                Neutral
+              </li>
+              <li>
+                <input onChange = {this.changeAnswer} type = "radio" name = {Object.keys(Queries)[j]} value = {4} checked = {this.state.response[Object.keys(Queries)[j]] === 4}  />
+                <br></br>
+                Slightly agree
+              </li>
+              <li>
+                <input onChange = {this.changeAnswer} type = "radio" name = {Object.keys(Queries)[j]} value = {5} checked = {this.state.response[Object.keys(Queries)[j]] === 5}  />
+                <br></br>
+                Agree
+              </li>
+            </ul>
+          </div>
+        );
+      }
     }
 
     return (
       <div className = "Test" >
+        <div style = {{height : '36px'}} ></div>
         <ul className = "Progress" >
           {progressLists}
         </ul>
         <form onSubmit = {this.submitForm} >
-          <div style = {basicStyle} className = "Basic" >
-            <label>Your age : </label>
-            <input style = {basicStyle} onChange = {this.changeAge} type = "number" value = {this.state.age} min = {1} max = {150} />
-          </div>
-          <div style = {basicStyle} className = "Basic" >
-            <label>Your gender : </label>
-            <select style = {basicStyle} onChange = {this.changeGender} value = {this.state.gender}>
-              <option value={0}>Rather not say</option>
-              <option value={1}>Male</option>
-              <option value={2}>Female</option>
-              <option value={3}>Other</option>
-            </select>
-          </div>
-          <div className = "Question" >
-            <label> I have to psych myself up before I am brave enough to make a phone call. </label>
-            
-          </div>
+          {
+            (this.state.currentPage === 0)
+            ? page[0]
+            : (this.state.currentPage === 1)
+              ? page[1]
+              : (this.state.currentPage === 2)
+                ? page[2]
+                : (this.state.currentPage === 3)
+                  ? page[3]
+                  : (this.state.currentPage === 4)
+                    ? page[4]
+                    : (this.state.currentPage === 5)
+                      ? page[5]
+                      : (this.state.currentPage === 6)
+                        ? page[6]
+                        : (this.state.currentPage === 7)
+                          ? page[7]
+                          : (this.state.currentPage === 8)
+                            ? page[8]
+                            : (this.state.currentPage === 9)
+                              ? page[9]
+                              : ""
+          }
+          <ul className = "navigation" style = {basicStyle} >
+            <li>
+              {
+                (this.state.currentPage === 0)
+                ? ""
+                : <button onClick = {this.goBack} style = {basicStyle} >Back</button>
+              }
+            </li>
+            <li style = {{fontWeight : '600'}} >Be honest!</li>
+            <li>
+              {
+                (this.state.currentPage === 9)
+                ? <button style = {inverseStyle } >Submit</button>
+                : <button onClick = {this.goNext} style = {basicStyle} >Next</button>
+              }
+            </li>
+          </ul>
         </form>
       </div>
     );
